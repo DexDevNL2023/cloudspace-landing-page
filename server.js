f65323c5810d40cb63004dade4977f008a3bc965
DexDevNL2023 <dexternlang@gmail.com>
2024-05-21 16:40:45 +0200

    devops2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/dist/service-manager-app'))
+app.use(express.static(__dirname + '/dist/cloudspace-landing-page'))
 
 app.get('/ping', function (req, res)
 {
@@ -13,48 +13,10 @@ app.get('/ping', function (req, res)
 
 app.get('/*', function (req, res)
 {
-    res.sendFile(path.join(__dirname + '/dist/service-manager-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/cloudspace-landing-page/index.html'));
 });
 
 app.listen(port, function ()
 {
     console.info('Angular Server App listening on port ' + port);
 });
-
-
-
-/*//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-//app.use(express.static('./dist/service-manager-app'));
-app.use(express.static(path.join(__dirname, 'dist','service-manager-app')));
-
-app.get('/!*', (req, res) =>
-    /!*res.sendFile('index.html', {root: 'dist/service-manager-app/'}),*!/
-    res.sendFile(path.join(__dirname,'dist','service-manager-app','index.html'))
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);*/
-
-
-
-/*//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
-
-app.get('/!*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);*/
